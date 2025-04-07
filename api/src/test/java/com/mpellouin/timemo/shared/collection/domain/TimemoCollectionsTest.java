9423442cdd3a2028358abc6d851c675248dfc95d
mpellouin <maxence.pellouin@epitech.eu>
2025-04-07 23:36:13 +0200

    feat(api): init jhipsterlite client

---
+++ api/src/test/java/com/mpellouin/timemo/shared/collection/domain/TimemoCollectionsTest.java
@@ -0,0 +1,115 @@
+package com.mpellouin.timemo.shared.collection.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import com.mpellouin.timemo.UnitTest;
+
+@UnitTest
+class TimemoCollectionsTest {
+
+  @Nested
+  @DisplayName("Collections")
+  class TimemoCollectionsCollectionsTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      Collection<Object> input = null;
+      Collection<Object> collection = TimemoCollections.immutable(input);
+
+      assertThat(collection).isEmpty();
+      assertThatThrownBy(collection::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      Collection<String> input = new ArrayList<>();
+      input.add("value");
+      Collection<String> collection = TimemoCollections.immutable(input);
+
+      assertThat(collection).containsExactly("value");
+      assertThatThrownBy(collection::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Set")
+  class TimemoCollectionsSetTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      Set<Object> input = null;
+      Set<Object> set = TimemoCollections.immutable(input);
+
+      assertThat(set).isEmpty();
+      assertThatThrownBy(set::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      Set<String> input = new HashSet<>();
+      input.add("value");
+      Set<String> set = TimemoCollections.immutable(input);
+
+      assertThat(set).containsExactly("value");
+      assertThatThrownBy(set::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("List")
+  class TimemoCollectionsListTest {
+
+    @Test
+    void shouldGetEmptyImmutableCollectionFromNullCollection() {
+      List<Object> input = null;
+      List<Object> list = TimemoCollections.immutable(input);
+
+      assertThat(list).isEmpty();
+      assertThatThrownBy(list::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableCollectionFromMutableCollection() {
+      List<String> input = new ArrayList<>();
+      input.add("value");
+      List<String> list = TimemoCollections.immutable(input);
+
+      assertThat(list).containsExactly("value");
+      assertThatThrownBy(list::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+
+  @Nested
+  @DisplayName("Map")
+  class TimemoMapTest {
+
+    @Test
+    void shouldGetEmptyImmutableMapFromNullMap() {
+      Map<Object, Object> input = null;
+      Map<Object, Object> map = TimemoCollections.immutable(input);
+
+      assertThat(map).isEmpty();
+      assertThatThrownBy(map::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    void shouldGetImmutableMapFromMutableMap() {
+      Map<String, String> input = new HashMap<>();
+      input.put("key", "value");
+      Map<String, String> map = TimemoCollections.immutable(input);
+
+      assertThat(map).containsExactly(Map.entry("key", "value"));
+      assertThatThrownBy(map::clear).isExactlyInstanceOf(UnsupportedOperationException.class);
+    }
+  }
+}
