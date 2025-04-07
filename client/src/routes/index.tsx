4a6ae6879851bf677e12311f885a66c0731b65dd
mpellouin <maxence.pellouin@epitech.eu>
2025-04-07 23:26:43 +0200

    feat(client): aded shadcn vintage paper

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
-import '../App.css'
+import '../styles.css'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -8,29 +7,8 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+    <div className='dark'>
+      <div className='h-[100vh] bg-background'></div>
     </div>
   )
 }
